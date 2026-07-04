e864b8f98107c268df70b42da9a3d3260c35f5fb
harumonia <zxjlm233@gmail.com>
2026-07-04 20:27:14 +0800

    feat: add album photo grid

---
+++ theme/components/ArgusAlbumGrid.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { ArgusAlbumPhoto } from '../types/albums'
+
+defineProps<{
+  photos: ArgusAlbumPhoto[]
+}>()
+
+const emit = defineEmits<{
+  select: [index: number]
+}>()
+
+function aspectStyle(photo: ArgusAlbumPhoto) {
+  if (photo.width && photo.height)
+    return { aspectRatio: `${photo.width} / ${photo.height}` }
+  return { aspectRatio: '4 / 3' }
+}
+</script>
+
+<template>
+  <p v-if="!photos.length" class="argus-album-empty">
+    这本相册还没有可展示的照片。
+  </p>
+
+  <div v-else class="argus-album-grid">
+    <button
+      v-for="(photo, index) in photos"
+      :key="photo.id"
+      class="argus-album-grid__item"
+      type="button"
+      :style="aspectStyle(photo)"
+      :aria-label="`打开照片 ${index + 1}`"
+      @click="emit('select', index)"
+    >
+      <img :src="photo.thumbnailPath" :alt="photo.originalFilename || `Album photo ${index + 1}`" loading="lazy">
+      <span v-if="photo.city || photo.capturedAt" class="argus-album-grid__caption">
+        {{ [photo.city, photo.capturedAt.slice(0, 10)].filter(Boolean).join(' · ') }}
+      </span>
+    </button>
+  </div>
+</template>
